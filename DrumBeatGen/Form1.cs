bfce1c342c2405daf1d100940ab72b0058e8db2d
Seth Onken <sonken@purdue.edu>
2018-02-04 15:27:14 -0500

    Implemented BPM functionality

--- DrumBeatGen/Form1.cs
+++ DrumBeatGen/Form1.cs
@@ -6,17 +6,60 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace DrumBeatGen {
     public partial class Form1 : Form {
         Pattern pattern;
+        bool restart; //used to end play thread when new pattern is created
+        System.Timers.Timer timer;
 
         public Form1() {
             InitializeComponent();
+            restart = false;
+            timer = new System.Timers.Timer();
+            timer.Elapsed += OnTimedEvent;
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e) {
+        private void play() {
+            while (pattern.getPlay()) {
+                for (int i = 0; i < pattern.getGridSize(); i++) {
+                    //set timer
+                    timer.Enabled = true;
+
+                    int tick = i / pattern.getTickMultiplier();
+
+                    //check if paused/new pattern generated
+                    while (pattern == null || !pattern.getPlay()) {
+                        if (restart) {
+                            //reset pattern bar location
+                            this.panel6.Location = new System.Drawing.Point(26, 376);
+                            this.Refresh();
+                            return;
+                        }
+                    }
+                    //move pattern bar location
+                    this.panel6.Location = new System.Drawing.Point(26 + (int)((i + 1) / (1.0 * pattern.getGridSize()) * 252), 376);
+                    this.Refresh();
+
+                    //loop to keep with bpm
+                    while (timer.Enabled) {
+                        ;
+                    }
+                }
+
+                //reset pattern bar location
+                this.panel6.Location = new System.Drawing.Point(26, 376);
+                this.Refresh();
+            }
+        }
+
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e) {
+            timer.Enabled = false;
+        }
+
+            private void trackBar1_Scroll(object sender, EventArgs e) {
             this.textBox3.Text = this.trackBar1.Value.ToString();
         }
 
@@ -37,6 +80,11 @@ namespace DrumBeatGen {
             //update the trackbar to reflect user input BPM
             this.trackBar1.Value = newBPM;
             this.textBox3.Text = this.trackBar1.Value.ToString();
+
+            //update new timer
+            if (pattern != null) {
+                updateTimer();
+            }
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e) {
@@ -78,6 +126,15 @@ namespace DrumBeatGen {
             //update button text
             if (pattern.getPlay()) {
                 this.button2.Text = "Pause";
+
+                //start beat
+                if (restart) {
+                    restart = false;
+                    System.Threading.ThreadStart work = play;
+                    System.Threading.Thread thread = new System.Threading.Thread(work);
+                    thread.IsBackground = true;
+                    thread.Start();
+                }
             }
             else {
                 this.button2.Text = "Play";
@@ -85,12 +142,61 @@ namespace DrumBeatGen {
         }
 
         private void button1_Click(object sender, EventArgs e) {
+            //generate new pattern
             pattern = null;
             pattern = new Pattern(this.comboBox1.Text, Int32.Parse(this.textBox3.Text), Int32.Parse(this.textBox5.Text), this.comboBox2.Text);
 
             //reset play button
             pattern.setPlay(false);
             this.button2.Text = "Play";
+            restart = true;
+
+            //set timer
+            updateTimer();
+
+            //draw snares on grid
+            List<int> snare = pattern.getSnareList();
+            foreach (int hit in snare) {
+                PictureBox pictureBox = copyPictureBox(this.pictureBox2);
+                pictureBox.Location = new System.Drawing.Point(pictureBox.Location.X + (int)((hit / (pattern.getGridSize() / (1.0 * pattern.getTickMultiplier()))) * 252), pictureBox.Location.Y);
+                this.Controls.Add(pictureBox);
+                pictureBox.BringToFront();
+            }
+
+            //draw kicks on grid
+            List<int> kick = pattern.getKickList();
+            foreach (int hit in kick) {
+                PictureBox pictureBox = copyPictureBox(this.pictureBox3);
+                pictureBox.Location = new System.Drawing.Point(pictureBox.Location.X + (int)((hit / (pattern.getGridSize() / (1.0 * pattern.getTickMultiplier()))) * 252), pictureBox.Location.Y);
+                this.Controls.Add(pictureBox);
+                pictureBox.BringToFront();
+            }
+
+            //bring pattern bar to front
+            this.panel6.BringToFront();
+        }
+
+        private PictureBox copyPictureBox(PictureBox pb) {
+            PictureBox pictureBox = new PictureBox();
+
+            pictureBox.BackColor = pb.BackColor;
+            pictureBox.Image = pb.Image;
+            pictureBox.Location = pb.Location;
+            pictureBox.Name = pb.Name;
+            pictureBox.Size = pb.Size;
+            pictureBox.SizeMode = pb.SizeMode;
+
+            return pictureBox;
+        }
+
+        private void updateBPM(object sender, EventArgs e) {
+            if (pattern != null) {
+                this.pattern.setBPM(this.trackBar1.Value);
+            }
+        }
+
+        private void updateTimer() {
+            timer.Interval = ((8.0 * 60000.0 / pattern.getBPM() / pattern.getGridSize()));
         }
     }
 }
--- DrumBeatGen/Pattern.cs
+++ DrumBeatGen/Pattern.cs
@@ -4,20 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Image credits
+ * -------------
+ * Kit - http://www.clker.com/clipart-4464.html
+ */
+
 namespace DrumBeatGen {
     public class Pattern {
+        //values from user input
         bool play;
         string timeSignature;
         int bpm;
         int complexity;
         string drumKit;
 
+        //values for grid size
+        int tickMultiplier;
+        int measures;
+        int noteLength;
+        int gridSize;
+
+        //values for note placements
+        List<int> snare;
+        List<int> kick;
+
         public Pattern(string timeSignature, int bpm, int complexity, string drumKit) {
+            //values from user input
             this.play = false;
             this.timeSignature = timeSignature;
             this.bpm = bpm;
             this.complexity = complexity;
             this.drumKit = drumKit;
+
+            //values for grid size
+            tickMultiplier = 1; // divisions of one note
+            measures = 2;
+            noteLength = 16; // 1/n note length
+            gridSize = tickMultiplier * measures * noteLength;
+
+            //values for note placements
+            snare = new List<int>();
+            kick = new List<int>();
         }
 
         public bool getPlay() {
@@ -58,6 +86,22 @@ namespace DrumBeatGen {
 
         public void setDrumKit(string drumKit) {
             this.drumKit = drumKit;
+        }
+
+        public int getTickMultiplier() {
+            return this.tickMultiplier;
+        }
+
+        public int getGridSize() {
+            return this.gridSize;
+        }
+
+        public List<int> getSnareList() {
+            return snare;
+        }
+
+        public List<int> getKickList() {
+            return kick;
         }
     }
 }
